57c6fd248ca23b10c7f1e70c036cccb52dd20d21
vitaladriano <adriano.mirandav@hotmail.com>
2020-02-04 16:08:34 +0000

    commit

---
+++ Lab2.js
@@ -0,0 +1,24 @@
+// Question 3
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+
+
+//Question 4
+function myAlphabetlength() {
+     console.log(myAlphabet.length)
+}
+myAlphabetlength();
+
+// Question 5
+var Planets = ['Earth','Neptune','Pluto','Saturn','Uranus'];
+// Question 6
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+
+//Question 7
+var wowDatatypes = ['32','adriano',6 < 7 ];
+console.log(wowDatatypes[0]);
+console.log(wowDatatypes[1]);
+console.log(wowDatatypes[2]);
